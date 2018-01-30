6adc4cce073728ef4041cf64ed04eb7fbe8e242e
junkdeck <fred@mslsr.com>
2018-01-30 19:30:30 +0100

    comments, usernames in console, send userlist to all users on connection/dc

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
+// cool stuff to implement:
+// "user is typing"
+// show currently online users
+
+function getUserIndexBySocketId(userArray, socketId){
+  // finds the array index of a given socket id
+  // takes an Object array with a '_id' key and a socket id
+  return userArray.map((user) => {
+    return user._id;
+  }).indexOf(socketId);
+}
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// keeps track of currently connected users - used for the "who's online" list in the gui
 var connected_users = new Array();
 
 app.get('/', (req,res) => {
+  // serves the HTML file to any client when connecting to *:3000
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connect', (socket) => {
+  // send login message to all users, save user in 'connected_users', update online list
   console.log(socket.id +" has connected.");
 
+  // stores socket id with nickname for display
   connected_users.push({
     _id: socket.id,
     nick: "user"
   });
 
-  console.log(JSON.stringify(connected_users));
-
   socket.on('client_connect', function(data){
+    // 'client_connect' is sent from the client directly after a connection to transfer user-specific data
+    // notify users of new connection
     io.emit('server_message', {msg: data.nick+' has connected'});
-
-    let user_id = connected_users.map((user) => {
-      return user._id;
-    }).indexOf(socket.id);
-
+    // finds the array index of the connected user's socket id
+    let user_id = getUserIndexBySocketId(connected_users, socket.id);
+    // updates the nickname on connection
     connected_users[user_id].nick = data.nick;
-
+    // distributes the updated userlist
+    io.emit('online_users_list', connected_users);
+    console.log(connected_users[user_id].nick + " connected");
   });
-  console.log("user connected");
 
   socket.on('disconnect', () => {
     // broadcast user disconnect and remove from 'connected users' list
@@ -36,18 +51,18 @@ io.on('connect', (socket) => {
     let user_id = connected_users.map((user) => {
       return user._id;
     }).indexOf(socket.id);
-    // delete the entry in 'connected users' list by slicing 'one' out
-    console.log("USER INDEX: "+user_id);
-    console.log(connected_users[user_id].nick+" disconnected");
 
+    // notifies users of disconnect
+    console.log(connected_users[user_id].nick+" disconnected");
     io.emit('server_message', {msg:connected_users[user_id].nick+' disconnected'});
-    io.emit('remove_dc_user', {_id: socket.id});
+    // removes the disconnected user
     connected_users.splice(user_id, 1);
+    // distributes the updated userlist
+    io.emit('online_users_list', connected_users);
   });
 
   socket.on('chat_message', (msg) => {
-    console.log("message: " + msg.msg);
-    console.log("from: " + socket.id);
+    console.log(msg.nick + ": " + msg.msg);
     socket.broadcast.emit('chat_message', msg);
   });
 });
